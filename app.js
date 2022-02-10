7e6ae81c2b2779ba183ae3935be86f78d829accd
khaled39ab <khaled39ab@gmail.com>
2022-02-10 13:45:53 +0600

    Displaying songs

--- app.js
+++ app.js
@@ -7,3 +7,29 @@ const searchSongs = () => {
         .then(data => displaySongs(data.data))
 }
 
+const displaySongs = songs => {
+    const songsContainer = document.getElementById("songs-container");
+    songsContainer.innerText = '';
+    
+    songs.forEach(song => {
+        // console.log(song);
+        const songDiv = document.createElement("div");
+        songDiv.className = "single-result row align-items-center my-3 p-3"
+        songDiv.innerHTML = `
+                    <div class="col-md-9">
+                        <h3 class="lyrics-name">${song.title}</h3>
+                        <p class="author lead">Album by <span>${song.album.title}</span></p>
+                        <audio controls>
+                            <source src="${song.preview}" type="audio/ogg">     
+                        </audio>
+                    </div>
+                    
+                    <div class="col-md-3 text-md-right text-center">
+                        <button onclick = "getLyrics('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
+                    </div>                   
+                    `;
+        songsContainer.appendChild(songDiv);
+
+    });
+};
+
